23f5919d612c8ae7c8fd684f3596b516498e158a
dpmulligan <dominic.p.mulligan@gmail.com>
2014-06-23 10:55:00 +0100

    redesign and reimplementation of mosquito starting based on feedback from itp referees

---
+++ src2/Mosquito/Main.hs
@@ -0,0 +1,10 @@
+module Mosquito.Main where
+
+  import Mosquito.Kernel.Dependent.Nat
+  import Mosquito.Kernel.Dependent.Vector
+  import Mosquito.Kernel.Kernel
+  import Mosquito.Kernel.Substitution
+  import Mosquito.Kernel.QName
+
+  main :: IO ()
+  main = return ()
